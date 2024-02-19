22320b126892ca401f4f9eb9973d5efbcbda576d
Amir-Ashraf <154739352+Amir-Asharf@users.noreply.github.com>
2024-02-19 18:55:56 +0200

    Update signup.dart

--- lib/project/signup.dart
+++ lib/project/signup.dart
@@ -11,83 +11,168 @@ class _signupState extends State<signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: Drawer(),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(child: Image.asset("images/13.jpg")),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Form(
-                  child: Column(
+       drawer: Drawer(),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          color: Colors.white,
+          child: ListView(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  TextFormField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(color: Colors.black)),
-                        border: OutlineInputBorder(),
-                        hintText: "username",
-                        prefixIcon: Icon(Icons.person),
-                        prefixIconColor: Colors.blue),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(color: Colors.black)),
-                        border: OutlineInputBorder(),
-                        hintText: "email",
-                        prefixIcon: Icon(Icons.person),
-                        prefixIconColor: Colors.blue),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide(color: Colors.black)),
-                        border: OutlineInputBorder(),
-                        hintText: "Password",
-                        prefixIcon: Icon(Icons.person),
-                        prefixIconColor: Colors.blue),
-                  ),
                   Container(
-                    margin: EdgeInsets.all(10),
-                    child: Row(
-                      children: [
-                        Text("if you have account "),
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context).pushNamed("login");
-                          },
-                          child: Text(
-                            "click Here",
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                        )
-                      ],
+                    height: 50,
+                  ),
+                  Center(
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 170,
+                      height: 170,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(80)),
+                      child: Image.asset(
+                        "images/13.jpg",
+                        width: 200,
+                        height: 200,
+                      ),
                     ),
                   ),
+                  Text(
+                    "register",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
                   Container(
-                      padding: EdgeInsets.all(15),
-                      child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pushNamed("homebage");
-                          },
-                          child: Text("انشاء الحساب"))),
+                    height: 10,
+                  ),
+                  Text(
+                    "Enter your personal information",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  Text(
+                    "username",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: ("Enter your name"),
+                        hintStyle: TextStyle(fontSize: 15, color: Colors.black),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 199, 199, 199)),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Text(
+                    "Email",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: ("Enter your Email"),
+                        hintStyle: TextStyle(fontSize: 15, color: Colors.black),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 199, 199, 199)),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Text(
+                    "password",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: ("Enter password"),
+                        hintStyle: TextStyle(fontSize: 15, color: Colors.black),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 199, 199, 199)),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  Text(
+                    "confirm password",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  Container(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: ("Enter confirm password"),
+                        hintStyle: TextStyle(fontSize: 15, color: Colors.black),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        filled: true,
+                        fillColor: Color.fromARGB(255, 199, 199, 199)),
+                  ),
                 ],
-              )),
-            )
-          ],
+              ),
+              Container(
+                height: 20,
+              ),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                height: 40,
+                textColor: Colors.white,
+                color: Colors.orange,
+                onPressed: () {
+                  Navigator.of(context).pushNamed("homebage");
+                },
+                child: Text(
+                  "Register",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              Container(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamed("login");
+                },
+                child: Center(
+                  child: Text.rich(TextSpan(children: [
+                    TextSpan(text: "Have an account? "),
+                    TextSpan(
+                        text: "login",
+                        style: TextStyle(
+                            color: Colors.orange, fontWeight: FontWeight.bold))
+                  ])),
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
+
